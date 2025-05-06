26b5968e77359ccc06853e9026d76d67c18d61c7
Rujit33 <rujit33@gmail.com>
2025-05-07 00:25:14 +0545

    vid-1:28:30

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
+
 const Nav = () => {
   const isUserLoggedIn = true;
 
--- components/provider.jsx
+++ components/provider.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+"use client";
 
-const Provider = () => {
-  return <div>provider</div>;
+import React from "react";
+import { SessionProvider } from "@node_modules/next-auth/react";
+const Provider = ({ children, session }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default Provider;
